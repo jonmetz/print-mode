78c0e3a4f3acc1a3356922d18983fbbcc201dd7f
Jonathan Metzman <jon.metzman@gmail.com>
2013-12-25 16:31:51 -0500

    Got working

--- print-mode.el
+++ print-mode.el
@@ -1,18 +1,41 @@
+(defvar print-mode-keymap (make-keymap) "print-mode keymap.")
+
+(define-minor-mode print-mode
+  :version "0.0.1"
+  :keymap print-mode-keymap)
+
 (setq counter 0)
+(setq mode (format "%s" major-mode))
+(define-key print-mode-keymap (kbd "C-c i") 'print-mode-insert-statement)
+(setq print-mode-statement "a")
 
-(let (mode (format "%" major-mode))
-  (if (equal mode  "emacs-lisp-mode")
-      (print "aaa")
-    (print "b")
-))
+;; (defun print-mode-set-statement ()
+;;   (if (equal mode "emacs-lisp-mode")
+;;       (setq print-mode-statement "\n(print %d)\n")
+;;     (setq print-mode-statement "// No valid mode detected %d\n")
+;;     ))
+(defun print-mode-set-statement ()
+  (if (equal mode "emacs-lisp-mode")
+      (setq print-mode-statement "\n(print %d)\n")
+    (if (equal mode "c-mode")
+        (setq print-statement "\nprintf(\"%d\n\");")
+      (if (equal mode "python-mode")
+          (setq print-statement "\nprint(%d)")
+        ("\n// No mode recognized"))))
 
-(defun insert-print-statement 
+(print-mode-set-statement)
+
+;; (defun print-mode-insert-statement ()
+;;   (interactive)
+;;   (progn 
+;;     (insert (format (print-mode-set-statement counter))
+;;     (message (format "print statement inserted on %s" (what-line)))
+;;     (setq counter (+ 1 counter)))))
+(defun print-mode-insert-statement ()
   (interactive)
   (progn 
-    (insert (format print-statement counter))
+    (insert (format print-mode-statement counter))
     (message (format "print statement inserted on %s" (what-line)))
-    (setq counter (+ 1 counter)))) 
-  
-;; (defun incrementing-print-statement ()
+    (setq counter (+ 1 counter))))
 
-
+(provide 'print-mode)
