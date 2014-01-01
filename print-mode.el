bb2b0b71c3a6cc7d3503883d728be2abef883d6e
Jonathan Metzman <jon.metzman@gmail.com>
2014-01-01 02:14:25 -0500

    Removed commented-out code

--- print-mode.el
+++ print-mode.el
@@ -8,24 +8,8 @@
 (setq mode (format "%s" major-mode))
 (define-key print-mode-keymap (kbd "C-c i") 'print-mode-insert-statement)
 
-;; (defun print-mode-set-statement ()
-;;   (if (equal mode "emacs-lisp-mode")
-;;       (setq print-mode-statement "\n(print %d)\n")
-;;     (setq print-mode-statement "// No valid mode detected %d\n")
-;;     ))
-
 (defun print-mode-create-statement (print-statement)
     (format "\n%s \n" (format print-statement "\nDebug Message: %d \n")))
-
-;; (defun print-mode-set-statement ()
-;;   (if (equal mode "emacs-lisp-mode")
-;;       (setq print-mode-statement "\n (print \"\\n %d \\n\") \n")
-;;     (if (equal mode "c-mode")
-;;         (setq print-statement "\nprintf(\"\\n %d \\n\");")
-;;       (if (equal mode "python-mode")
-;;           (setq print-statement "\nprint('\\n %d \\n')")
-;;         ("\n// No mode recognized")))))
-
 (defun print-mode-set-statement ()
   (if (equal mode "emacs-lisp-mode")
       (setq print-mode-statement (print-mode-create-statement "(print \"%s\")"))
@@ -37,12 +21,7 @@
 
 (print-mode-set-statement)
 
-;; (defun print-mode-insert-statement ()
-;;   (interactive)
-;;   (progn 
-;;     (insert (format (print-mode-set-statement counter))
-;;     (message (format "print statement inserted on %s" (what-line)))
-;;     (setq counter (+ 1 counter)))))
+
 (defun print-mode-insert-statement ()
   (interactive)
   (progn 
